416940c625c1fdfdb74368ec7baf3b3b2037722e
Nico <83230980+NicolasTu-dev@users.noreply.github.com>
2024-09-20 16:13:31 -0300

    Initial commit

---
+++ NicolasTu/src/main/java/com/challengeTecnico/AutoModos/NicolasTu/NicolasTuApplication.java
@@ -0,0 +1,13 @@
+package com.challengeTecnico.AutoModos.NicolasTu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicolasTuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicolasTuApplication.class, args);
+	}
+
+}
---
+++ NicolasTu/src/test/java/com/challengeTecnico/AutoModos/NicolasTu/NicolasTuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengeTecnico.AutoModos.NicolasTu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasTuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
